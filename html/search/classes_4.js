db9792c5675b86931c61b89275129a02b912a3ec
Celina Vang Henningsen <civah@hotmail.dk>
2024-11-07 22:34:03 +0100

    Doxy update

--- html/search/classes_4.js
+++ html/search/classes_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['meleeattackitem_0',['MeleeAttackItem',['../class_mandatory2_d_game_framework_1_1models_1_1attack_1_1_melee_attack_item.html',1,'Mandatory2DGameFramework::models::attack']]],
-  ['movestate_1',['MoveState',['../class_mandatory2_d_game_framework_1_1models_1_1_creatures_1_1_states_1_1_move_state.html',1,'Mandatory2DGameFramework::models::Creatures::States']]]
+  ['movestate_1',['MoveState',['../class_mandatory2_d_game_framework_1_1models_1_1_creatures_1_1_states_1_1_move_state.html',1,'Mandatory2DGameFramework::models::Creatures::States']]],
+  ['mylogger_2',['MyLogger',['../class_mandatory2_d_game_framework_1_1_models_1_1_my_logger.html',1,'Mandatory2DGameFramework::Models']]]
 ];
